ec470884c638a9a106eb53c70d907accaaaab0a6
AbduvoiotovAsadbek <abduvoitovasadbek3@gmail.com>
2021-09-25 02:57:17 +0500

    Cretaed Table

--- src/App.js
+++ src/App.js
@@ -1,25 +1,165 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+import React, { Component } from "react";
+import { data } from "./mock.js";
+export default class App extends Component {
+  state = {
+    data: data,
+    name: "",
+    newName: "",
+    status: "",
+    newStatus: "",
+    selected: null,
+    newData: data,
+    prop: "",
+  };
+  render() {
+    const onChange = (e) => {
+      this.setState({
+        [e.target.name]: e.target.value,
+      });
+    };
+    const onEdit = (e) => {
+      this.setState({
+        name: e.name,
+        status: e.status,
+        selected: e.id,
+      });
+    };
+    const onSave = (e) => {
+      let newData = this.state.data.map((value) =>
+        value.id === this.state.selected
+          ? { ...value, name: this.state.name, status: this.state.status }
+          : value
+      );
+      this.setState({
+        data: newData,
+        selected: null,
+      });
+    };
+    const onDelete = (e) => {
+      let map = this.state.data.filter((item) => item.id !== e.id);
+      this.setState({
+        data: map,
+      });
+    };
+    const onAdd = () => {
+      if (
+        this.state.newName.length !== 0 &&
+        this.state.newStatus.length !== 0
+      ) {
+        let newUser = {
+          id: this.state.data[this.state.data.length - 1].id + 1,
+          name: this.state.newName,
+          status: this.state.newStatus,
+        };
+        var Pushdata = this.state.data;
+        Pushdata.push(newUser);
+        this.setState({
+          newName: "",
+          newStatus: "",
+        });
+      } else {
+        prompt("Are you full these inputs?");
+      }
+    };
+    return (
+      <div>
+        <table
+          border="1"
+          style={{
+            borderCollapse: "collapse",
+            width: "70%",
+            margin: " auto",
+            marginTop: "100px",
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          <thead>
+            <tr>
+              <th>
+                <input
+                  style={{ width: "95%" }}
+                  name="newName"
+                  onChange={onChange}
+                  value={this.state.newName}
+                />
+              </th>
+              <th>
+                <input
+                  style={{ width: "95%" }}
+                  name="newStatus"
+                  onChange={onChange}
+                  value={this.state.newStatus}
+                />
+              </th>
+              <th>New Status</th>
+              <th>
+                <button onClick={onAdd}>Add</button>
+                <button
+                  onClick={() => this.setState({ data: this.state.newData })}
+                >
+                  Restore
+                </button>
+              </th>
+            </tr>
+          </thead>
+        </table>
+        <table
+          border="1"
+          style={{
+            borderCollapse: "collapse",
+            width: "70%",
+            margin: "auto",
+          }}
+        >
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>NAME</th>
+              <th>STATUS</th>
+              <th>ACTION {this.state.data.length}</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.data.map((value) => {
+              return (
+                <tr>
+                  <td>{value.id}</td>
+                  <td>
+                    {this.state.selected === value.id ? (
+                      <input
+                        name="name"
+                        onChange={onChange}
+                        value={this.state.name}
+                      />
+                    ) : (
+                      value.name
+                    )}
+                  </td>
+                  <td>
+                    {this.state.selected === value.id ? (
+                      <input
+                        name="status"
+                        onChange={onChange}
+                        value={this.state.status}
+                      />
+                    ) : (
+                      value.status
+                    )}
+                  </td>
+                  <td>
+                    {this.state.selected === value.id ? (
+                      <button onClick={onSave}>Save</button>
+                    ) : (
+                      <button onClick={() => onEdit(value)}>Edit</button>
+                    )}
+                    <button onClick={() => onDelete(value)}>Delete</button>
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+        {console.log(this.state.data)}
+      </div>
+    );
+  }
 }
-
-export default App;
---
+++ src/mock.js
@@ -0,0 +1,6 @@
+export const data = [
+  { id: 1, name: "Asadbek", status: "Front-End-Developer" },
+  { id: 2, name: "Muhammad", status: "Backend-Developer" },
+  { id: 3, name: "Sherzod", status: "Backend-Developer" },
+  { id: 4, name: "Davlat", status: "Front-End-Developer" },
+];
